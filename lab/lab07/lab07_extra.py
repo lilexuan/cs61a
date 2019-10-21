5c54cb2b4db14336b56f70af153d0f7b7097ab5c
kelvin <834524281@qq.com>
2019-10-21 21:49:11 +0800

    lab07 finished

--- lab/lab07/lab07_extra.py
+++ lab/lab07/lab07_extra.py
@@ -17,6 +17,15 @@ def has_cycle(link):
     False
     """
     "*** YOUR CODE HERE ***"
+    def helper(link, have_seen=[]):
+        if link is Link.empty:
+            return False
+        elif link in have_seen:
+            return True
+        else:
+            have_seen.append(link)
+            return helper(link.rest)
+    return helper(link)
 
 def has_cycle_constant(link):
     """Return whether link contains a cycle.
@@ -30,6 +39,17 @@ def has_cycle_constant(link):
     False
     """
     "*** YOUR CODE HERE ***"
+    if link is Link.empty:
+        return False
+    slow, fast = link, link.rest
+    while fast is not Link.empty:
+        if fast.rest == Link.empty:
+            return False
+        elif fast == slow or fast.rest == slow:
+            return True
+        else:
+            slow, fast = slow.rest, fast.rest.rest
+    return False
 
 def reverse_other(t):
     """Mutates the tree such that nodes on every other (odd-depth) level
@@ -45,5 +65,13 @@ def reverse_other(t):
     Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
     """
     "*** YOUR CODE HERE ***"
-    cnt = 0
+    def reverse_helper(t, need_reverse):
+        if t.is_leaf():
+            return 
+        new_labs = [b.label for b in t.branches][::-1]
+        for i in range(len(t.branches)):
+            reverse_helper(t.branches[i], not need_reverse)
+            if need_reverse:
+                t.branches[i].label = new_labs[i]
+    reverse_helper(t, True)
     
