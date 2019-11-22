e23bb67d3c77d2a717add937af60686a66e5ab38
kelvin <834524281@qq.com>
2019-11-22 18:18:12 +0800

    lab10 q5 finished

--- lab/lab10/expr.py
+++ lab/lab10/expr.py
@@ -110,6 +110,10 @@ class Name(Expr):
         Exception raised!
         """
         "*** YOUR CODE HERE ***"
+        if self.string in env.keys():
+            return env[self.string]
+        else:
+            raise NameError()
 
     def __str__(self):
         return self.string
